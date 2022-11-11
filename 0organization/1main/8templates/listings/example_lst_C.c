8e2b595669e92f56adfd627d2f165dfa25c7471b
DenKrysos <51788226+DenKrysos@users.noreply.github.com>
2022-11-11 10:51:45 +0100

    Updates from Overleaf

---
+++ 0organization/1main/8templates/listings/example_lst_C.c
@@ -0,0 +1,25 @@
+void* ¢\color{eclipse_keyword2}\bfseries some\_function¢(void* args, int argc){
+	if(var->entry){
+		char* some_point = NULL;
+		int len = ¢\color{eclipse_keyword2}make\_something¢($some_point);
+		while(current != var->iterator){
+			//Some Comment
+		}
+		free(some_point);
+	}else{
+		switch(¢\color{eclipse_keyword2}get\_type¢(some_point)){
+		case val1:{
+			¢\color{eclipse_keyword2}another\_func¢(input);
+			break;
+		}
+		case val2:{
+			//Do something else
+			break;
+		}
+		default:{
+			break;
+		}
+		}//end switch (type)
+	}
+	return;
+}
