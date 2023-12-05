f6bacd90a618b401d1b1156cc82d880b7c7b93e3
DenKrysos <51788226+DenKrysos@users.noreply.github.com>
2023-12-05 16:46:29 +0100

    Updates from Overleaf

--- 0organization/1main/8templates/listings/example_lst_C.c
+++ 0organization/1main/8templates/listings/example_lst_C.c
@@ -1,7 +1,7 @@
-void* ¢\color{black}\bfseries some\_function¢(void* args, int argc){
+void* ¢\lstfuncdeclplain{some_function}¢(void* args, int argc){
 	if(var->entry){
 		char* some_point = NULL;
-		int len = ¢\color{eclipse_keyword2}make\_something¢($some_point);
+		int len = ¢\lstCfunc{make_something}¢($some_point);
 		struct timespec tim;
 		tim->tv_nsec=200;
 		struct some_struct struc;
@@ -11,9 +11,9 @@ void* ¢\color{black}\bfseries some\_function¢(void* args, int argc){
 		}
 		free(some_point);
 	}else{
-		switch(¢\color{eclipse_keyword2}get\_type¢(some_point)){
+		switch(¢\lstCfunc{get_type}¢(some_point)){
 		case val1:{
-			¢\color{eclipse_keyword2}another\_func¢(input);
+			¢\lstCfunc{another_func}¢(input);
 			break;
 		}
 		case val2:{
